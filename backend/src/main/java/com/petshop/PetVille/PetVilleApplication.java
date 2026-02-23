0310ebd3b476309311b63235be265596a932b8c8
andreyindras <andreyindras55@gmail.com>
2026-02-23 11:49:10 -0300

    inicializando projeto

---
+++ backend/src/main/java/com/petshop/PetVille/PetVilleApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.PetVille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetVilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetVilleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/petshop/PetVille/PetVilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.PetVille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetVilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
