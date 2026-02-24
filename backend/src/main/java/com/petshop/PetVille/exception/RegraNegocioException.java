d01263dcf5082d716a93c32923d831f9bf06f824
andreyindras <andreyindras55@gmail.com>
2026-02-24 15:33:24 -0300

    criação de exception

---
+++ backend/src/main/java/com/petshop/PetVille/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package com.petshop.PetVille.exception;
+
+public class RegraNegocioException extends RuntimeException {
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
